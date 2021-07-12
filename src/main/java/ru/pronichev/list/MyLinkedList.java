4ac68e0cf2928abc6bdd06be298b34e38f09bd69
Aleksei Pronichev <aleksei.pronichev@outlook.com>
2021-07-12 16:33:12 +0300

    add LinkedList with interface List

---
+++ src/main/java/ru/pronichev/list/MyLinkedList.java
@@ -0,0 +1,123 @@
+package ru.pronichev.list;
+
+import ru.pronichev.collections.MyList;
+
+public class MyLinkedList<T> implements MyList<T> {
+
+    private Node<T> firstElement;
+    private Node<T> lastElement;
+    private int size;
+
+    public MyLinkedList() {
+        this.firstElement = null;
+        this.lastElement = null;
+        this.size = 0;
+    }
+
+    @Override
+    public void add(T element) {
+        Node<T> node = new Node<>(element);
+        if (size == 0) {
+            firstElement = node;
+        } else {
+            lastElement.setNext(node);
+        }
+        lastElement = node;
+        size++;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        if (index < 0 || index > size) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        if (size == 0) {
+            add(element);
+        } else {
+            Node<T> current = firstElement;
+            for (var i = 0; i < index-1; i++ ) {
+                current = current.getNext();
+            }
+            Node<T> newElement = new Node<>(element);
+            Node<T> next = current.getNext();
+            current.setNext(newElement);
+            newElement.setNext(next);
+            size++;
+        }
+    }
+
+    @Override
+    public T get(int index) {
+        Node<T> current = firstElement;
+        for (var i = 0; i < index; i++ ) {
+            current = current.getNext();
+        }
+        return current.getValue();
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        Node<T> current = firstElement;
+        for (var i = 0; i < index-1; i++ ) {
+            current = current.getNext();
+        }
+        Node<T> returnValue = current.getNext();
+        current.setNext(returnValue.getNext());
+        size--;
+        return returnValue.getValue();
+    }
+
+    @Override
+    public T removeValue(T element) {
+        if (element == null) {
+            throw new IllegalArgumentException("argument should be not null");
+        }
+        Node<T> previous = firstElement;
+        Node<T> current = firstElement;
+        while (!current.getValue().equals(element)) {
+            previous = current;
+            current = current.getNext();
+
+            if (current == null) {
+                throw new IllegalArgumentException("not found");
+            }
+        }
+
+        previous.setNext(current.getNext());
+        size--;
+        return current.getValue();
+    }
+
+    private static class Node<T> {
+        private Node<T> next;
+        private T value;
+
+        public Node(T value) {
+            this.value = value;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+    }
+}
---
+++ src/test/java/ru/pronichev/list/MyLinkedListTest.java
@@ -0,0 +1,86 @@
+package ru.pronichev.list;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class MyLinkedListTest {
+    private static final int TEST_ARRAY_SIZE = 25;
+
+    private int[] values;
+    private MyLinkedList<Integer> myLinkedList;
+
+    @BeforeEach
+    void initValues() {
+        this.values = new int[TEST_ARRAY_SIZE];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = i;
+        }
+         myLinkedList = new MyLinkedList<>();
+        for (int value : values) {
+            myLinkedList.add(value);
+        }
+    }
+
+    @Test
+    void add() {
+        int value = 3;
+        int size = myLinkedList.size();
+        myLinkedList.add(value);
+
+        assertEquals(size + 1, myLinkedList.size());
+        assertEquals(value, myLinkedList.get(size));
+    }
+
+    @Test
+    void addIndex() {
+        int insertValue = 20;
+        int insertIndex = 3;
+        myLinkedList.add(insertIndex, insertValue);
+
+        System.out.println();
+        assertEquals(insertValue, myLinkedList.get(insertIndex));
+    }
+
+    @Test
+    void size() {
+        assertEquals(values.length, myLinkedList.size());
+    }
+
+    @Test
+    void get() {
+        for (int i = 0; i < values.length; i++) {
+            assertEquals(values[i], myLinkedList.get(i));
+        }
+    }
+
+    @Test
+    void removeIndex() {
+        int index = 3;
+
+        int startSize = myLinkedList.size();
+        int startValue = myLinkedList.get(3);
+
+        myLinkedList.remove(index);
+
+        assertEquals(startSize - 1, myLinkedList.size());
+        assertNotEquals(startValue, myLinkedList.get(index));
+    }
+
+    @Test
+    void removeValue() {
+        Object value = 3;
+
+        int startSize = myLinkedList.size();
+
+        myLinkedList.removeValue((Integer) value);
+
+        assertEquals(startSize - 1, myLinkedList.size());
+
+        for (int i = 0; i < myLinkedList.size(); i++) {
+            assertNotEquals(value, myLinkedList.get(i));
+        }
+    }
+}
